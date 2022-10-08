f70488829118f0aafff6e20cc854297596bec73f
dip887 <gdeepanshu289@gmail.com>
2022-10-08 12:09:10 +0530

    ra

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
@@ -9,6 +10,7 @@ const app = express();
 //middleware
 // this method is to parse the incoming requests with json payloads
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
